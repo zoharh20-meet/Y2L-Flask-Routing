b6be96b885750d19708d489e4dfa64a7977ca18d
myname21-meet <myname21@meet.mit.edu>
2019-11-07 11:44:31 -0500

    flask

--- app.py
+++ app.py
@@ -6,9 +6,21 @@ app.secret_key = "MY_SUPER_SECRET_KEY"
 
 
 ##### Code here ######
+@app.route('/')
+def first_route():
+	return render_template("home.html")
 
+@app.route('/about')
+def about():
+	return render_template("about.html")
 
+@app.route('/store')
+def store():
+	return render_template("store.html")
 
+@app.route('/cart')
+def store():
+	return render_template("cart.html")
 #####################
 
 
